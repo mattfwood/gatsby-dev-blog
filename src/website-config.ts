0a47c1fb8651abd890779efd6654688f53996340
Matt Wood <matt@cratebind.com>
2019-11-12 12:00:17 -0600

    Remove extra items from config

--- src/website-config.ts
+++ src/website-config.ts
@@ -55,14 +55,14 @@ const config: WebsiteConfig = {
   logo: 'img/ghost-logo.png',
   lang: 'en',
   siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
-  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
-  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
-  mailchimpEmailFieldName: 'MERGE0',
-  googleSiteVerification: 'GoogleCode',
-  footer: 'is based on Gatsby Casper',
+  // facebook: 'https://www.facebook.com/ghost',
+  // twitter: 'https://twitter.com/tryghost',
+  showSubscribe: false,
+  // mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  // mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  // mailchimpEmailFieldName: 'MERGE0',
+  // googleSiteVerification: 'GoogleCode',
+  // footer: 'is based on Gatsby Casper',
 };
 
 export default config;
